d808f7e8b72fe6d755db2912ef1b6fdb91576da2
Bruinxs <huangxs@itdayang.com>
2016-11-13 14:06:50 +0800

    add: value fetch

---
+++ uv/value.go
@@ -0,0 +1,277 @@
+package uv
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type Value interface {
+	Value(interface{}) interface{}
+}
+
+type Fetcher Value
+
+//fetch value by the format string,format define how to get value,it separate rows by ';'
+//for a row,separated by ','
+//var,m,n
+//first option is a variable name
+//second, m: must be have the variable, o: it is optional
+//third define the range
+//'n': no range
+//number: a int greater or equal then number,a string length greater or equal then number
+//num1~num2: a int greater or equal then num1, and lesser or equal then num2
+//val1|val2|val3: the variable value must be val1 or val2 or val3
+func Fetch(valuer Value, format string, args ...interface{}) error {
+	format = strings.Trim(format, " \r\n\t")
+	if format[len(format)-1] == ';' {
+		format = format[:len(format)-1]
+	}
+	rows := strings.Split(format, ";")
+	if len(rows) != len(args) {
+		return fmt.Errorf("format rows len(%v) not equal arg len(%v)", len(rows), len(args))
+	}
+
+	size := len(rows)
+	for i := 0; i < size; i++ {
+		rows[i] = strings.Trim(rows[i], " \r\n\t")
+		options := strings.Split(rows[i], ",")
+		if len(options) != 3 {
+			return fmt.Errorf("row(%v) option len(%v) not equal 3", rows[i], len(options))
+		}
+
+		val := valuer.Value(options[0])
+		if val == nil {
+			if options[1] == "o" {
+				continue
+			} else if options[1] == "m" {
+				return fmt.Errorf("row(%v) variable(%v) must be provide", rows[i], options[0])
+			} else {
+				return fmt.Errorf("row(%v) second option(%v) is illegal", rows[i], options[1])
+			}
+		}
+
+		argVal := reflect.ValueOf(args[i])
+		if argVal.Kind() != reflect.Ptr {
+			return fmt.Errorf("arg in order %v expect ptr type but a %t", i, args[i])
+		}
+
+		var err error
+		elemVal := argVal.Elem()
+		switch elemVal.Kind() {
+		case reflect.String:
+			if sv, ok := val.(string); ok {
+				elemVal.SetString(sv)
+			} else {
+				elemVal.SetString(fmt.Sprintf("%v", val))
+			}
+
+			err = checkRange(elemVal, options[2])
+			if err != nil {
+				return fmt.Errorf("row(%v), %v", rows[i], err)
+			}
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			var iv int64
+			switch val.(type) {
+			case int:
+				iv = int64(val.(int))
+			case int8:
+				iv = int64(val.(int8))
+			case int16:
+				iv = int64(val.(int16))
+			case int32:
+				iv = int64(val.(int32))
+			case int64:
+				iv = val.(int64)
+			case string:
+				s := val.(string)
+				iv, err = strconv.ParseInt(s, 10, 64)
+				if err != nil {
+					return fmt.Errorf("row(%v) parse str val(%v) to int64 err(%v)", rows[i], s, err)
+				}
+			default:
+				return fmt.Errorf("row(%v) val(%v) assert to int fail", rows[i], val)
+			}
+			elemVal.SetInt(iv)
+
+			err = checkRange(elemVal, options[2])
+			if err != nil {
+				return fmt.Errorf("row(%v), %v", rows[i], err)
+			}
+		case reflect.Float32, reflect.Float64:
+			var fv float64
+			switch val.(type) {
+			case float32:
+				fv = float64(val.(float32))
+			case float64:
+				fv = val.(float64)
+			case string:
+				s := val.(string)
+				fv, err = strconv.ParseFloat(s, 64)
+				if err != nil {
+					return fmt.Errorf("row(%v) parse str val(%v) to float64 err(%v)", rows[i], s, err)
+				}
+			default:
+				return fmt.Errorf("row(%v) val(%v) assert to float fail", rows[i], val)
+			}
+			elemVal.SetFloat(fv)
+
+			err = checkRange(elemVal, options[2])
+			if err != nil {
+				return fmt.Errorf("row(%v), %v", rows[i], err)
+			}
+		case reflect.Bool:
+			switch val.(type) {
+			case bool:
+				elemVal.SetBool(val.(bool))
+			case int:
+				iv := val.(int)
+				elemVal.SetBool(iv != 0)
+			case string:
+				s := val.(string)
+				b, err := strconv.ParseBool(s)
+				if err != nil {
+					return fmt.Errorf("row(%v) parse str val(%v) to bool err(%v)", rows[i], s, err)
+				}
+				elemVal.SetBool(b)
+			default:
+				return fmt.Errorf("row(%v) val(%v) assert to bool fail", rows[i], val)
+			}
+		case reflect.Slice:
+			switch val.(type) {
+			case string:
+				s := val.(string)
+				slice := strings.Split(s, ",")
+				elemSlice := elemVal
+				elemSlice = reflect.AppendSlice(elemSlice, reflect.ValueOf(slice))
+				elemVal.Set(elemSlice)
+			default:
+				return fmt.Errorf("row(%v) val(%v) assert to slice fail", rows[i], val)
+			}
+		default:
+			return fmt.Errorf("row(%v) arg(%v) type(%t) illegal", rows[i], args[i], args[i])
+		}
+	}
+	return nil
+}
+
+func checkRange(val reflect.Value, ranges string) error {
+	ranges = strings.Trim(ranges, " ")
+	if ranges == "" {
+		return fmt.Errorf("val(%v) of range is empty", val)
+	}
+	if ranges == "n" {
+		return nil
+	}
+
+	var rangeStrV []string
+	var rangeFV []float64
+	var flg int
+	var err error
+
+	if strings.Contains(ranges, "~") {
+		rangeStrV = strings.Split(ranges, "~")
+		if len(rangeStrV) != 2 {
+			return fmt.Errorf("ranges(%v) illegal, should only have two val both ends of '~'", ranges)
+		}
+		flg = 1
+		rangeFV = make([]float64, 2)
+		rangeFV[0], err = strconv.ParseFloat(rangeStrV[0], 64)
+		if err != nil {
+			return fmt.Errorf("ranges(%v) parse str(%v) to float err(%v)", ranges, rangeStrV[0], err)
+		}
+		rangeFV[1], err = strconv.ParseFloat(rangeStrV[1], 64)
+		if err != nil {
+			return fmt.Errorf("ranges(%v) parse str(%v) to float err(%v)", ranges, rangeStrV[1], err)
+		}
+	} else if strings.Contains(ranges, "|") {
+		rangeStrV = strings.Split(ranges, "|")
+		flg = 2
+	} else {
+		flg = 3
+		rangeFV = make([]float64, 1)
+		rangeFV[0], err = strconv.ParseFloat(ranges, 64)
+		if err != nil {
+			return fmt.Errorf("ranges(%v) parse to float err(%v)", ranges, err)
+		}
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		sv := val.String()
+		switch flg {
+		case 1, 3:
+			if len(sv) < int(rangeFV[0]) {
+				return fmt.Errorf("str val(%v) len(%v) less than %v, not in range(%v)", sv, len(sv), rangeFV[0], ranges)
+			}
+			if flg == 1 && len(sv) > int(rangeFV[1]) {
+				return fmt.Errorf("str val(%v) len(%v) greater than %v, not in range(%v)", sv, len(sv), rangeFV[1], ranges)
+			}
+		case 2:
+			ill := true
+			for _, s := range rangeStrV {
+				if sv == s {
+					ill = false
+					break
+				}
+			}
+			if ill {
+				return fmt.Errorf("str val(%v) not in range(%v)", sv, ranges)
+			}
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		iv := val.Int()
+		switch flg {
+		case 1, 3:
+			if iv < int64(rangeFV[0]) {
+				return fmt.Errorf("int val(%v) less than %v, not in range(%v)", iv, rangeFV[0], ranges)
+			}
+			if flg == 1 && iv > int64(rangeFV[1]) {
+				return fmt.Errorf("int val(%v) greater than %v, not in range(%v)", iv, rangeFV[1], ranges)
+			}
+		case 2:
+			ill := true
+			for _, s := range rangeStrV {
+				k, err := strconv.ParseInt(s, 10, 64)
+				if err != nil {
+					return fmt.Errorf("parse str(%v) to int err(%v)", s, err)
+				}
+				if k == iv {
+					ill = false
+					break
+				}
+			}
+			if ill {
+				return fmt.Errorf("int val(%v) not in range(%v)", iv, ranges)
+			}
+		}
+	case reflect.Float32, reflect.Float64:
+		fv := val.Float()
+		switch flg {
+		case 1, 3:
+			if fv < rangeFV[0] {
+				return fmt.Errorf("float val(%v) less than %v, not in range(%v)", fv, rangeFV[0], ranges)
+			}
+			if flg == 1 && fv > rangeFV[1] {
+				return fmt.Errorf("float val(%v) greater than %v, not in range(%v)", fv, rangeFV[1], ranges)
+			}
+		case 2:
+			ill := true
+			for _, s := range rangeStrV {
+				f, err := strconv.ParseFloat(s, 64)
+				if err != nil {
+					return fmt.Errorf("parse str(%v) t float err(%v)", s, err)
+				}
+				if f == fv {
+					ill = false
+					break
+				}
+			}
+			if ill {
+				return fmt.Errorf("float val(%v) not in range(%v)", fv, ranges)
+			}
+		}
+	}
+	return nil
+}
---
+++ uv/value_test.go
@@ -0,0 +1,351 @@
+package uv
+
+import (
+	"fmt"
+	"github.com/Bruinxs/tu"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+type MV map[string]interface{}
+
+func (mv MV) Value(key interface{}) interface{} {
+	if str, ok := key.(string); ok {
+		return mv[str]
+	}
+	panic(fmt.Sprintf("key(%T) ill", key))
+}
+
+func TestCheckRange(t *testing.T) {
+	tvl := []struct {
+		Val   interface{}
+		Range string
+		Ok    bool
+	}{
+		{nil, "  ", false},
+		{nil, "n", true},
+		{nil, "1~2~3", false},
+		{nil, "s1~2", false},
+		{nil, "1~s2", false},
+		{nil, "str", false},
+		//
+		{10, "10", true},
+		{10, "11", false},
+		{10, "10~11", true},
+		{10, "9~10", true},
+		{10, "1~9", false},
+		{10, "11~19", false},
+		{10, "1|2|10", true},
+		{10, "1|2|100", false},
+		{10, "s1|s2|s3", false},
+		{10.98, "10.98", true},
+		{10.98, "10.99", false},
+		{10.98, "10.98~11", true},
+		{10.98, "10~10.98", true},
+		{10.98, "10~10.97", false},
+		{10.98, "11.78~12", false},
+		{10.98, "100.87|120.23|10.98", true},
+		{10.98, "10.97|10.99|100.01", false},
+		{10.98, "s1|s2", false},
+		{"str", "3", true},
+		{"str", "4", false},
+		{"str", "3~4", true},
+		{"str", "2~3", true},
+		{"str", "0~1", false},
+		{"str", "4~90", false},
+		{"str fake", "str|str fake", true},
+		{"str", "s1|s2|s3", false},
+	}
+	for _, tv := range tvl {
+		err := checkRange(reflect.ValueOf(tv.Val), tv.Range)
+		if (err == nil) != tv.Ok {
+			t.Errorf("tv(%v), err(%v)", tv, err)
+			return
+		}
+	}
+}
+
+func TestFetch(t *testing.T) {
+	m := MV{
+		"s1": "str",
+		"s2": "str2",
+		"s4": "str1,str2,str3,str4",
+		//
+		"i1": 2,
+		"f1": 3.14159,
+		"b1": true,
+		//
+		"i8":  int8(8),
+		"i16": int16(16),
+		"i32": int32(32),
+		"i64": int64(64),
+		"is":  "100",
+		//
+		"f32": float32(32.14),
+		"fs":  "3.1415",
+		//
+		"bs": "true",
+	}
+	var (
+		s1 string
+		s3 string = "s3"
+		s4 []string
+	)
+
+	//1,must
+	err := Fetch(m, `
+		s3,m,0;
+	`, &s3)
+	if err == nil || !strings.Contains(err.Error(), "must be provide") {
+		t.Errorf("err(%v) is illegal", err)
+		return
+	}
+
+	//2,range
+	err = Fetch(m, "s1,m,4", &s1)
+	if err == nil || !strings.Contains(err.Error(), "not in range") {
+		t.Errorf("err(%v) is illegal", err)
+		return
+	}
+
+	//3,ptr
+	err = Fetch(m, "s1,m,2", s1)
+	if err == nil || !strings.Contains(err.Error(), "expect ptr type") {
+		t.Errorf("err(%v) is illegal", err)
+		return
+	}
+
+	//4,arg
+	err = Fetch(m, "s1,m,2")
+	if err == nil || !strings.Contains(err.Error(), "not equal arg len") {
+		t.Errorf("err(%v) is illegal", err)
+		return
+	}
+
+	//5
+	err = Fetch(m, `
+		s1,m,3;
+		s3,o,0;
+		s4,m,n;
+	`, &s1, &s3, &s4)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if got, want := s1, m["s1"].(string); got != want {
+		t.Errorf("s1(%v) != %v", got, want)
+		return
+	}
+	if got, want := s3, "s3"; got != want {
+		t.Errorf("s3(%v) != %v", got, want)
+		return
+	}
+	if got, want := s4, []string{"str1", "str2", "str3", "str4"}; !tu.CmpStr_Strict(got, want) {
+		t.Errorf("s4(%v) != %v", got, want)
+		return
+	}
+
+	//6
+	var (
+		i1 int
+		f1 float64
+		b1 bool
+	)
+
+	err = Fetch(m, `
+		i1,o,2~3;
+		f1,m,2.14|3.14159;
+		b1,m,n;
+	`, &i1, &f1, &b1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if got, want := i1, 2; got != want {
+		t.Errorf("i1(%v) != %v", got, want)
+		return
+	}
+	if got, want := f1, 3.14159; got != want {
+		t.Errorf("f1(%v) != %v", got, want)
+		return
+	}
+	if got, want := b1, true; got != want {
+		t.Errorf("b1(%v) != %v", got, want)
+		return
+	}
+
+	//7
+	//string
+	err = Fetch(m, "i1,m,1;", &s1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := s1, "2"; got != want {
+		t.Errorf("s1(%v) != %v", got, want)
+		return
+	}
+
+	//int
+	err = Fetch(m, "i8,m,0;", &i1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := i1, 8; got != want {
+		t.Errorf("i1(%v) != %v", got, want)
+		return
+	}
+
+	err = Fetch(m, "i16,m,0;", &i1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := i1, 16; got != want {
+		t.Errorf("i1(%v) != %v", got, want)
+		return
+	}
+
+	err = Fetch(m, "i32,m,0;", &i1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := i1, 32; got != want {
+		t.Errorf("i1(%v) != %v", got, want)
+		return
+	}
+
+	err = Fetch(m, "i64,m,0;", &i1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := i1, 64; got != want {
+		t.Errorf("i1(%v) != %v", got, want)
+		return
+	}
+
+	err = Fetch(m, "is,m,0;", &i1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := i1, 100; got != want {
+		t.Errorf("i1(%v) != %v", got, want)
+		return
+	}
+
+	//float
+	err = Fetch(m, "f32,m,0;", &f1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := f1, float64(float32(32.14)); got != want {
+		t.Errorf("f1(%v) != %v", got, want)
+		return
+	}
+
+	err = Fetch(m, "fs,m,0;", &f1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := f1, 3.1415; got != want {
+		t.Errorf("f1(%v) != %v", got, want)
+		return
+	}
+
+	//bool
+	b1 = false
+	err = Fetch(m, "i1,m,0;", &b1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := b1, true; got != want {
+		t.Errorf("b1(%v) != %v", got, want)
+		return
+	}
+
+	b1 = false
+	err = Fetch(m, "bs,m,0;", &b1)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := b1, true; got != want {
+		t.Errorf("b1(%v) != %v", got, want)
+		return
+	}
+
+	//8 option err
+	err = Fetch(m, "s1,m;", &s1)
+	if err == nil || !strings.Contains(err.Error(), "not equal 3") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "t1,f,n;", &s1)
+	if err == nil || !strings.Contains(err.Error(), "second option") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "s1,m,0;", &i1)
+	if err == nil || !strings.Contains(err.Error(), "to int64 err") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "b1,m,0;", &i1)
+	if err == nil || !strings.Contains(err.Error(), "assert to int fail") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "i1,m,0|1;", &i1)
+	if err == nil || !strings.Contains(err.Error(), "row(i1,m,0|1)") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "s1,m,n;", &f1)
+	if err == nil || !strings.Contains(err.Error(), "to float64 err") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "b1,m,n;", &f1)
+	if err == nil || !strings.Contains(err.Error(), "assert to float fail") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "f1,m,0|0;", &f1)
+	if err == nil || !strings.Contains(err.Error(), "row(f1,m,0|0)") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "s1,m,0|0;", &b1)
+	if err == nil || !strings.Contains(err.Error(), "to bool err") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "f1,m,0|0;", &b1)
+	if err == nil || !strings.Contains(err.Error(), "assert to bool fail") {
+		t.Error(err)
+		return
+	}
+
+	err = Fetch(m, "f1,m,0|0;", &s4)
+	if err == nil || !strings.Contains(err.Error(), "assert to slice fail") {
+		t.Error(err)
+		return
+	}
+
+	var (
+		m1 map[string]int
+	)
+	err = Fetch(m, "f1,m,0|0;", &m1)
+	if err == nil || !strings.Contains(err.Error(), "illegal") {
+		t.Error(err)
+		return
+	}
+}
