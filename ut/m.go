135ed41e817ff3ba793b05c554af43e83ea698ba
bruinxs <bruinxs@gmail.com>
2017-07-02 15:39:21 +0800

    add: 支持用路径方式获取M的深度值

--- ut/m.go
+++ ut/m.go
@@ -1,6 +1,13 @@
 package ut
 
-import "github.com/bruinxs/util/uv"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/bruinxs/util/uv"
+)
 
 type M map[string]interface{}
 
@@ -12,6 +19,19 @@ func (m M) Exist(key string) bool {
 func (m M) Str(key string) string {
 	val, ok := m[key]
 	if !ok {
+		return ""
+	}
+
+	sv, err := uv.I2Str(val)
+	if err != nil {
+		return ""
+	}
+	return sv
+}
+
+func (m M) StrP(patch string) string {
+	val := m.ValP(patch)
+	if val == nil {
 		return ""
 	}
 
@@ -35,13 +55,43 @@ func (m M) Int64(key string) int64 {
 	return iv
 }
 
+func (m M) Int64P(patch string) int64 {
+	val := m.ValP(patch)
+	if val == nil {
+		return 0
+	}
+
+	iv, err := uv.I2Int64(val)
+	if err != nil {
+		return 0
+	}
+	return iv
+}
+
 func (m M) Int(key string) int {
 	return int(m.Int64(key))
+}
+
+func (m M) IntP(patch string) int {
+	return int(m.Int64P(patch))
 }
 
 func (m M) Float(key string) float64 {
 	val, ok := m[key]
 	if !ok {
+		return 0
+	}
+
+	fv, err := uv.I2Float64(val)
+	if err != nil {
+		return 0
+	}
+	return fv
+}
+
+func (m M) FloatP(patch string) float64 {
+	val := m.ValP(patch)
+	if val == nil {
 		return 0
 	}
 
@@ -65,6 +115,19 @@ func (m M) Bool(key string) bool {
 	return bv
 }
 
+func (m M) BoolP(patch string) bool {
+	val := m.ValP(patch)
+	if val == nil {
+		return false
+	}
+
+	bv, err := uv.I2Bool(val)
+	if err != nil {
+		return false
+	}
+	return bv
+}
+
 func (m M) StrSlice(key string) []string {
 	val, ok := m[key]
 	if !ok {
@@ -76,4 +139,69 @@ func (m M) StrSlice(key string) []string {
 		return nil
 	}
 	return ssv
+}
+
+func (m M) StrSliceP(patch string) []string {
+	val := m.ValP(patch)
+	if val == nil {
+		return nil
+	}
+
+	ssv, err := uv.I2StrSlice(val)
+	if err != nil {
+		return nil
+	}
+	return ssv
+}
+
+func (m M) ValP(path string) interface{} {
+	path = strings.Trim(path, "/")
+	if path == "" {
+		return nil
+	}
+
+	ps := strings.Split(path, "/")
+	val, ok := m[ps[0]]
+	if !ok {
+		return nil
+	}
+	vVal := reflect.ValueOf(val)
+	for _, key := range ps[1:] {
+		kind := vVal.Kind()
+		for kind == reflect.Ptr || kind == reflect.Interface {
+			if vVal.IsNil() {
+				return nil
+			}
+			vVal = vVal.Elem()
+			kind = vVal.Kind()
+		}
+
+		switch kind {
+		case reflect.Map:
+			vVal = vVal.MapIndex(reflect.ValueOf(key))
+		case reflect.Slice, reflect.Array:
+			vLen := vVal.Len()
+			if key == "len" {
+				return vLen
+			} else if key == "last" {
+				if vLen == 0 {
+					return nil
+				}
+				vVal = vVal.Index(vLen - 1)
+			} else {
+				index, err := strconv.Atoi(key)
+				if err != nil {
+					panic(fmt.Sprintf("M valp try to convert key(%v) to integer fail with %v by path(%v)", key, err, path))
+				}
+				vVal = vVal.Index(index)
+			}
+		case reflect.Struct:
+			vVal = vVal.FieldByName(key)
+		}
+	}
+
+	if !vVal.IsValid() {
+		return nil
+	}
+	return vVal.Interface()
 }
--- ut/m_test.go
+++ ut/m_test.go
@@ -1,14 +1,17 @@
 package ut_test
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
-	"github.com/Bruinxs/util/ut"
+	. "github.com/Bruinxs/util/ut"
 	"github.com/bruinxs/ts"
+	"github.com/bruinxs/util/ut"
 )
 
 func TestM(t *testing.T) {
-	m := ut.M{}
+	m := M{}
 
 	//exist
 	if g, w := m.Exist("fake"), false; g != w {
@@ -122,5 +125,167 @@ func TestM(t *testing.T) {
 	if g, w := m.StrSlice("sl1"), []string{"s4", "s5", "s6"}; !ts.CmpStr_Strict(g, w) {
 		t.Errorf("got(%v) != %v", g, w)
 		return
+	}
+}
+
+func TestM_StrP(t *testing.T) {
+	type Tmp struct {
+		Key1 string
+		Key2 string
+		Key3 int
+		Key4 interface{}
+	}
+
+	type args struct {
+		patch string
+	}
+	tests := []struct {
+		name string
+		m    M
+		args args
+		want string
+	}{
+		{"", M{"k1": "v1"}, args{"k1"}, "v1"},
+		{"", M{"k1": "v1"}, args{"/k1"}, "v1"},
+		{"", M{"k1": M{"k2": "v2"}}, args{"k1/k2"}, "v2"},
+		{"", M{"k1": M{"k2": "v2"}}, args{"k1/k2/"}, "v2"},
+		{"", M{"k1": &M{"k2": "v2"}}, args{"k1/k2/"}, "v2"},
+		{"", M{"k1": [3]string{"v1", "v2", "v3"}}, args{"k1/0"}, "v1"},
+		{"", M{"k1": [3]string{"v1", "v2", "v3"}}, args{"k1/1"}, "v2"},
+		{"", M{"k1": [3]string{"v1", "v2", "v3"}}, args{"k1/2"}, "v3"},
+		{"", M{"k1": [3]string{"v1", "v2", "v3"}}, args{"k1/last"}, "v3"},
+		{"", M{"k2": []string{"v1", "v2", "v3"}}, args{"k2/0"}, "v1"},
+		{"", M{"k2": []string{"v1", "v2", "v3"}}, args{"k2/1"}, "v2"},
+		{"", M{"k2": []string{"v1", "v2", "v3"}}, args{"k2/2"}, "v3"},
+		{"", M{"k2": []string{"v1", "v2", "v3"}}, args{"k2/last"}, "v3"},
+		{"", M{"k": Tmp{"v1", "v2", 3, nil}}, args{"k/Key1"}, "v1"},
+		{"", M{"k": Tmp{"v1", "v2", 3, nil}}, args{"k/Key3"}, "3"},
+		{"", M{"k": &Tmp{"v1", "v2", 3, nil}}, args{"k/Key2"}, "v2"},
+		{"", M{"k": []M{{"arr": []interface{}{interface{}(&Tmp{"", "", 0, M{"kk": "kkv"}})}}}}, args{"k/last/arr/0/Key4/kk"}, "kkv"},
+		{"", M{"k": Tmp{"", "", 0, &Tmp{"", "", 0, M{"k2": "vs"}}}}, args{"k/Key4/Key4/k2"}, "vs"},
+		{"", M{"k": Tmp{"", "", 0, &Tmp{"", "", 0, M{}}}}, args{"k/Key4/Key4/k2/k3/k4"}, ""},
+	}
+	for _, tt := range tests {
+		tt.name = fmt.Sprintf("run %v", tt.m)
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.StrP(tt.args.patch); got != tt.want {
+				t.Errorf("M.StrP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestM_Int64P(t *testing.T) {
+	type args struct {
+		patch string
+	}
+	tests := []struct {
+		name string
+		m    M
+		args args
+		want int64
+	}{
+		{"0", M{"k": 123}, args{"k"}, 123},
+		{"1", M{"k": []int64{1, 2, 3}}, args{"k/last"}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.Int64P(tt.args.patch); got != tt.want {
+				t.Errorf("M.Int64P() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestM_IntP(t *testing.T) {
+	type args struct {
+		patch string
+	}
+	tests := []struct {
+		name string
+		m    M
+		args args
+		want int
+	}{
+		{"0", M{"k": 1}, args{"k"}, 1},
+		{"1", M{"k": []int{0, 1, 2}}, args{"k/1"}, 1},
+		{"2", M{"k": []int{0, 1, 2}}, args{"k/len"}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.IntP(tt.args.patch); got != tt.want {
+				t.Errorf("M.IntP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestM_FloatP(t *testing.T) {
+	type args struct {
+		patch string
+	}
+	tests := []struct {
+		name string
+		m    M
+		args args
+		want float64
+	}{
+		{"0", M{"k": 1.23}, args{"k"}, 1.23},
+		{"1", M{"k": []float64{1.1, 2.2}}, args{"k/0"}, 1.1},
+		{"2", M{"k": []float64{1.1, 2.2}}, args{"k/last"}, 2.2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.FloatP(tt.args.patch); got != tt.want {
+				t.Errorf("M.FloatP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestM_BoolP(t *testing.T) {
+	type args struct {
+		patch string
+	}
+	tests := []struct {
+		name string
+		m    M
+		args args
+		want bool
+	}{
+		{"0", M{"k": true}, args{"k"}, true},
+		{"1", M{"k": struct{ I int }{1}}, args{"k/I"}, true},
+		{"2", M{"k": []string{"true"}}, args{"k/0"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.BoolP(tt.args.patch); got != tt.want {
+				t.Errorf("M.BoolP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestM_StrSliceP(t *testing.T) {
+	type args struct {
+		patch string
+	}
+	tests := []struct {
+		name string
+		m    M
+		args args
+		want []string
+	}{
+		{"0", M{"k": "v1"}, args{"k"}, []string{"v1"}},
+		{"1", M{"k": "v1,v2,v3"}, args{"k"}, []string{"v1", "v2", "v3"}},
+		{"2", M{"k": []string{"v1", "v2"}}, args{"k"}, []string{"v1", "v2"}},
+		{"3", M{"k": [][]string{[]string{"v4", "v5"}}}, args{"k/0"}, []string{"v4", "v5"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.StrSliceP(tt.args.patch); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("M.StrSliceP() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
